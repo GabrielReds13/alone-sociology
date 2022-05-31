b7b702a3300a89245b6fdcdefeff519342324724
GabrielReds13 <92747698+GabrielReds13@users.noreply.github.com>
2022-05-31 16:13:59 -0400

    Function Menu

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+function openMenuRight() {
+  document.getElementById('menuRight').style.width = '18em'
+}
+
+function closeMenuRight() {
+  document.getElementById('menuRight').style.width = '0em'
+}
