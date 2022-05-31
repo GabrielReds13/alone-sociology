517b5d51b0cc3536c36d3a82efe1fdb9f86a9d53
GabrielReds13 <92747698+GabrielReds13@users.noreply.github.com>
2022-05-31 16:16:57 -0400

    Delete

--- main.js
+++
@@ -1,7 +0,0 @@
-function openMenuRight() {
-  document.getElementById('menuRight').style.width = '18em'
-}
-
-function closeMenuRight() {
-  document.getElementById('menuRight').style.width = '0em'
-}
